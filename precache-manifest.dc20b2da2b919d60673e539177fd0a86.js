67471afd9206d74c2bb1a549e05aa869053f9f04
Katic Nikola <nikolakatic1@gmail.com>
2019-07-22 22:10:36 +0200

    Updates

---
+++ precache-manifest.dc20b2da2b919d60673e539177fd0a86.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "989f290225339d26b4db5a5ca998b28a",
+    "url": "/react-to-do-app/index.html"
+  },
+  {
+    "revision": "df43cb68ac30bb1c9908",
+    "url": "/react-to-do-app/static/css/main.2be6bcdd.chunk.css"
+  },
+  {
+    "revision": "b3bd7909bbf519129ccc",
+    "url": "/react-to-do-app/static/js/2.87af8787.chunk.js"
+  },
+  {
+    "revision": "df43cb68ac30bb1c9908",
+    "url": "/react-to-do-app/static/js/main.16c41517.chunk.js"
+  },
+  {
+    "revision": "92d52159f3623a56a9a3",
+    "url": "/react-to-do-app/static/js/runtime~main.3e954007.js"
+  }
+]);
